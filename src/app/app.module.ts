3c444c8d543877be93bb80d8c96a1aa4275ac76e
Alejandro González Reyes <alexglzrys@gmail.com>
2022-03-23 13:05:31 -0600

    preparar componentes por defecto para pruebas de integración

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,22 @@ import { AppComponent } from './app.component';
 import { MedicosComponent } from './intermedio/espias/medicos.component';
 import { MedicoComponent } from './integracion-basicos/medico/medico.component';
 import { HttpClientModule } from '@angular/common/http';
+import { HospitalComponent } from './integracion-basicos/hospital/hospital.component';
+import { FormsModule } from '@angular/forms';
+import { IncrementadorComponent } from './integracion-basicos/incrementador/incrementador.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MedicosComponent,
-    MedicoComponent
+    MedicoComponent,
+    HospitalComponent,
+    IncrementadorComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
