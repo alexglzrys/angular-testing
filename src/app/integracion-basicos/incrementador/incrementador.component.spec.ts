f7db70b2f044bd87ac35a04d457cb4885c713328
Alejandro González Reyes <alexglzrys@gmail.com>
2022-03-23 13:38:50 -0600

    revisar el valor del input físicamente

--- src/app/integracion-basicos/incrementador/incrementador.component.spec.ts
+++ src/app/integracion-basicos/incrementador/incrementador.component.spec.ts
@@ -4,12 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { By } from '@angular/platform-browser';
 
 
-describe('Incremendator Component', () => {
+describe('Incrementador Component', () => {
 
     let component: IncrementadorComponent;
     let fixture: ComponentFixture<IncrementadorComponent>;
 
-    beforeEach( () => {
+    beforeEach(() => {
         TestBed.configureTestingModule({
             declarations: [ IncrementadorComponent ],
             imports: [ FormsModule ]
@@ -32,6 +32,21 @@ describe('Incremendator Component', () => {
       const el: HTMLElement = fixture.debugElement.query(By.css('h3')).nativeElement;
 
       expect(el.innerHTML).toContain('Proceso de carga');
+    });
+
+    it('Debe mostrar en el input el valor del progreso', () => {
+      // Cambiar el valor actual en la propiedad de progreso
+      component.cambiarValor(5);
+      // Activar la detección de cambios
+      fixture.detectChanges();
+
+      // ! La detección de cambios puede demorar un poco, es por ello que se debe continuar cuando el fixture sea estable
+      fixture.whenStable().then(() => {
+        const el: HTMLInputElement = fixture.debugElement.query(By.css('input')).nativeElement;
+        // Si por defecto tenía 50, entonces ahora debe ser 55
+        expect(el.value).toBe('55');
+      });
+
     });
 
 });
