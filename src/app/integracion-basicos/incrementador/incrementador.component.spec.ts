3df1d3b3d87d63d90024eec143f49580c87d9279
Alejandro González Reyes <alexglzrys@gmail.com>
2022-03-23 13:19:40 -0600

    realizar pruebas en el HTML del componente

--- src/app/integracion-basicos/incrementador/incrementador.component.spec.ts
+++ src/app/integracion-basicos/incrementador/incrementador.component.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed, ComponentFixture } from '@angular/core/testing';
 import { IncrementadorComponent } from './incrementador.component';
 import { FormsModule } from '@angular/forms';
+import { By } from '@angular/platform-browser';
 
 
 describe('Incremendator Component', () => {
@@ -19,9 +20,18 @@ describe('Incremendator Component', () => {
 
     });
 
-    it('', () => {
+    it('Debe mostrar la leyenda', () => {
+      // Asignar un valor a la propiedad leyenda del componente
+      component.leyenda = 'Proceso de carga';
 
+      // ? Es necesario indicar a Angular que dispare el detector de cambios, pues ha cambiado el valor de algunas propiedades y pueda pintarlas nuevamente en la UI
+      fixture.detectChanges();
 
+      // Obtener la referencia al elemento HTML que deseamos inspeccionar
+      // En este caso podemos buscar por CSS, directiva
+      const el: HTMLElement = fixture.debugElement.query(By.css('h3')).nativeElement;
+
+      expect(el.innerHTML).toContain('Proceso de carga');
     });
 
 });
